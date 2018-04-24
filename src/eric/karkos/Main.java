e3660f923bcf32315828760112f6a3f2870df0c4
Eric Karkos <ekarkos@rams.colostate.edu>
2018-04-24 11:43:07 -0600

    Created methods in Main to generate a random array of integers.

--- src/eric/karkos/Main.java
+++ src/eric/karkos/Main.java
@@ -1,8 +1,36 @@
 package eric.karkos;
 
+import java.util.Random;
+
 public class Main {
 
+    private Random random = new Random();
+
+    // Pre: The maximum size that you want the array returned to be
+    // Post: Returns a random integer between 0 and maxSize
+    private int getRandomSize(int maxSize) {
+        return random.nextInt(maxSize);
+    }
+
+    // Pre: int of the maximum size of the elements
+    // Post: Returns a random number between 0 and ceiling
+    // Ex. If ceiling := 500 will return a random int between 0 and 500 (exclusive)
+    private int getRandomElements(int ceiling) {
+        return random.nextInt(ceiling);
+    }
+
+    // Pre: the maximum size the array (maxSize) can be and the maximum element (ceiling)
+    // Post: Returns an int array of a random size and filled with random elements
+    // 0 <= randomArray.size < ceiling
+    public int[] createInts(int maxSize, int ceiling) {
+        int[] randomArray = new int[getRandomSize(maxSize)];
+        for (int i=0;i<randomArray.length;i++) {
+            randomArray[i] = getRandomElements(ceiling);
+        }
+        return randomArray;
+    }
+
     public static void main(String[] args) {
-	// write your code here
+
     }
 }
